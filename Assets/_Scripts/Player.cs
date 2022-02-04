ed90f9866203d9e7712aa48117ba65e01bd54da7
pablovass <faktory.fm@live.com>
2022-02-04 14:58:14 -0300

    corregit los asteroides

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -1,30 +1,39 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float xMin, xMax, zMin, zMax;
     public GameObject shot;
-    public Transform shotContent;
+    public Transform shotSpawn;
+    public GameObject asteroid;
+    public Vector3 spawnValues;
+    public int hazartCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
     public float fireRate;
     public float nextFire;
-    
 
+    public float xMin, xMax, zMin, zMax;
+    public Transform shotContent;
+    public int asteroidCount;
     private Rigidbody _rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        //instaciamos creacion de asteroides 
+        //StartCoroutine(createAsteroids());
+        StartCoroutine(SpawnWaves());
     }
 
     private void Update()
     {
-        if (Input.GetButton("Fire1")&& Time.time>nextFire)
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
-            Instantiate(shot,shotContent.position,shotContent.rotation);
+            Instantiate(shot, shotContent.position, shotContent.rotation);
         }
     }
 
@@ -38,6 +47,28 @@ public class Player : MonoBehaviour
 
         _rigidbody.position = new Vector3(Mathf.Clamp(_rigidbody.position.x, xMin, xMax), 6.0f,
             Mathf.Clamp(_rigidbody.position.z, zMin, zMax));
-        _rigidbody.rotation=Quaternion.Euler(0.0f,0.0f,_rigidbody.velocity.x*-2);
+        _rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, _rigidbody.velocity.x * -2);
     }
+
+    
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazartCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x.));
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(asteroid, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+
+            }
+
+            yield return new WaitForSeconds(waveWait);
+        }
+
+    }
+
 }
