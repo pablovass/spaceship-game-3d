b826c738f976f3828407b81c5b1f1e03b491b896
pablovass <faktory.fm@live.com>
2022-02-10 00:48:55 -0300

    asteroid update done

--- Assets/_Scripts/Parallax.cs
+++ Assets/_Scripts/Parallax.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//hace que el backgroun tenga vida
 public class Parallax : MonoBehaviour
 {
-public float speed=0;
+    public float speed=0;
 
     // Start is called before the first frame update
     void Start()
